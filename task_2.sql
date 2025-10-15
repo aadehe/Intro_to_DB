9ef515eb79f655090108223a4c2c7335ed6108bd
Albert ADEHE <aadehe@gmail.com>
2025-10-15 12:10:42 +0000

    modify code to capitalize the entities

--- task_2.sql
+++ task_2.sql
@@ -1,36 +1,36 @@
-CREATE TABLE books (
+CREATE TABLE Books (
     book_id INT PRIMARY KEY NOT NULL,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
     price DECIMAL(7, 2) NOT NULL,
     publication_date DATE NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE authors (
+CREATE TABLE Authors (
     author_id INT PRIMARY KEY NOT NULL,
     author_name VARCHAR(215) NOT NULL
 );
 
-CREATE TABLE customers (
+CREATE TABLE Customers (
     customer_id INT PRIMARY KEY NOT NULL,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL UNIQUE,
     address TEXT NOT NULL
 );
 
-CREATE TABLE orders (
+CREATE TABLE Orders (
     order_id INT PRIMARY KEY NOT NULL,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE order_details (
+CREATE TABLE Order_Details (
     order_detail_id INT PRIMARY KEY NOT NULL,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
