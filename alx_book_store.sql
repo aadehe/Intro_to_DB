069d2cab56234d3cd9ef41d7be50877f59d38fd9
Albert ADEHE <aadehe@gmail.com>
2025-10-15 11:10:56 +0000

    update foriegn key to match uppercase letter 'Customer'

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -38,7 +38,7 @@ CREATE TABLE orders (
     order_id INT PRIMARY KEY NOT NULL,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Order_Details: Stores information about the books included in each order.
